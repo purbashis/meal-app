09d3d34c118f0ca9ab62a0f76ea1b16685bef16a
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2023-10-30 00:07:52 +0530

    adding cross screen navigation

--- meals/lib/screens/categories.dart
+++ meals/lib/screens/categories.dart
@@ -1,35 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (ctx) => MealsScreen(
+                  title: 'Some title',
+                  meals: [],
+                )));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(
-      title: const Text('Pick your category'),
-
-    ),
-    body:GridView(
-      padding: const EdgeInsets.all(24),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 3/2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20)
-      
-      
-      
-    
-      ,
-      children: [//alternative by using map 
-              // availableCategories
-              // .map((category) => CategoryGridItem(category: category))
-              // .toList(),
-        for(final category in availableCategories )
-        CategoryGridItem(category: category)
-      ],
-    ) ,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick your category'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(24),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+        children: [
+          //alternative by using map
+          // availableCategories
+          // .map((category) => CategoryGridItem(category: category))
+          // .toList(),
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context);
+              },
+            )
+        ],
+      ),
     );
   }
 }
--- meals/lib/widgets/category_grid_item.dart
+++ meals/lib/widgets/category_grid_item.dart
@@ -2,31 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:meals/models/category.dart';
 
 class CategoryGridItem extends StatelessWidget {
-  const CategoryGridItem({super.key,required this.category});
+  const CategoryGridItem({super.key, 
+  required this.category,
+  required this.onSelectCategory,});
 
   final Category category;
+  final void Function() onSelectCategory;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: (){},
-      splashColor:Theme.of(context).primaryColor ,
-borderRadius: BorderRadius.circular(16),
+      onTap: onSelectCategory,
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(16),
 
-      // in inWell u get a feedback but if i used GestureDetector then you get no feedback 
+      // in inWell u get a feedback but if i used GestureDetector then you get no feedback
       child: Container(
         padding: const EdgeInsets.all(16),
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          gradient: LinearGradient(colors: [category.color.withOpacity(0.55),
-          category.color.withOpacity(0.9)],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight
-          )
+            borderRadius: BorderRadius.circular(16),
+            gradient: LinearGradient(colors: [
+              category.color.withOpacity(0.55),
+              category.color.withOpacity(0.9)
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
         ),
-        child:Text(category.title,style: Theme.of(context).textTheme.titleLarge!.copyWith(
-          color: Theme.of(context).colorScheme.onBackground,
-        ),),
       ),
     );
   }
