aa4182ea6c3c806372c3f71ef767410f68a5419c
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2023-10-31 19:13:41 +0530

    adding navigation to the MealDetails Screen

--- meals/lib/widgets/meal_item.dart
+++ meals/lib/widgets/meal_item.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:meals/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
-
+import 'package:meals/widgets/meal_item_trait.dart';
 import 'package:meals/models/meal.dart';
 
 class MealItem extends StatelessWidget {
   const MealItem({
     super.key,
     required this.meal,
+    required this.onSelectMeal,
   });
 
   final Meal meal;
+  final void Function(Meal meal) onSelectMeal;
 
   String get complexityText {
     return meal.complexity.name[0].toUpperCase() +
@@ -33,7 +34,9 @@ class MealItem extends StatelessWidget {
       clipBehavior: Clip.hardEdge,
       elevation: 2,
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          onSelectMeal(meal);
+        },
         child: Stack(
           children: [
             FadeInImage(
