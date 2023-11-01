fb6a8a781219b9fb9e40c5ccef28dafa074225db
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2023-11-01 20:41:15 +0530

    adding Tab based navigation

--- meals/lib/screens/categories.dart
+++ meals/lib/screens/categories.dart
@@ -26,11 +26,7 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Pick your category'),
-      ),
-      body: GridView(
+    return  GridView(
         padding: const EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
@@ -48,7 +44,7 @@ class CategoriesScreen extends StatelessWidget {
               },
             )
         ],
-      ),
+      
     );
   }
 }
--- meals/lib/screens/meals.dart
+++ meals/lib/screens/meals.dart
@@ -7,15 +7,14 @@ import 'package:meals/widgets/meal_item.dart';
 class MealsScreen extends StatelessWidget {
   const MealsScreen({
     super.key,
-    required this.title,
+    this.title,
     required this.meals,
   });
 
-  final String title;
+  final String? title;
   final List<Meal> meals;
 
   void selectMeal(BuildContext context, Meal meal) {
-  
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (ctx) => MealDetailsScreen(
@@ -60,9 +59,13 @@ class MealsScreen extends StatelessWidget {
       );
     }
 
+    if (title == null) {
+      return content;
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(title!),
       ),
       body: content,
     );
