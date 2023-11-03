a85d06ebc4fd8848927fe26facfe687b25c6660a
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2023-11-03 19:15:35 +0530

    using filter screen toggle button

--- meals/lib/screens/tabs.dart
+++ meals/lib/screens/tabs.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
-
+import 'package:meals/screens/filters.dart';
 import 'package:meals/screens/categories.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/main_drawer.dart';
@@ -43,6 +43,8 @@ class _TabsScreenState extends State<TabsScreen> {
 
   void _setScreen(String identifier) {
     if (identifier == 'filters') {
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (ctx) => const FiltersScreen()));
     } else {
       Navigator.of(context).pop();
     }
