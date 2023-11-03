08ec76afc1165351cb86b313ea80586772126e48
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2023-11-03 15:27:52 +0530

    marked as fav

--- meals/lib/screens/tabs.dart
+++ meals/lib/screens/tabs.dart
@@ -18,13 +18,25 @@ class _TabsScreenState extends State<TabsScreen> {
 
   final List<Meal> _favoriteMeals = [];
 
+  void _showInfoMessage(String message) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
+  }
+
   void _toggleMealFavoriteStatus(Meal meal) {
     final isExisting = _favoriteMeals.contains(meal);
 
     if (isExisting == true) {
-      _favoriteMeals.remove(meal);
+      setState(() {
+        _favoriteMeals.remove(meal);
+      });
+      _showInfoMessage('Meal is no longer a favorite');
     } else {
-      _favoriteMeals.add(meal);
+      setState(() {
+        _favoriteMeals.add(meal);
+        _showInfoMessage('Marked as Favorite');
+      });
     }
   }
 
@@ -43,7 +55,7 @@ class _TabsScreenState extends State<TabsScreen> {
 
     if (_selectedPageIndex == 1) {
       activePage = MealsScreen(
-        meals: [],
+        meals: _favoriteMeals,
         onTOggleFavorite: _toggleMealFavoriteStatus,
       );
       activePageTitle = 'Your Favorites';
